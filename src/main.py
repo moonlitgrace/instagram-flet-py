c7399dd2d10323db098b0b1e118dd1af8d672251
tokitou-san <tokitousan2005@gmail.com>
2023-07-27 20:29:55 +0530

    feat: add `MDApp` with hot reload

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from kivymd.tools.hotreload.app import MDApp
+from kivymd.uix.label import MDLabel
+
+class MainApp(MDApp):
+    def __init__(self, *args, **kwargs):
+        super(MainApp, self).__init__(*args, **kwargs)
+
+    def build_app(self):
+        return MDLabel(text="Instagram")
+
+if __name__ == "__main__":
+    MainApp().run()
+        
