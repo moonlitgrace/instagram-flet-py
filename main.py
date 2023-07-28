9ae269d31492e7838de19c0efab14baf8863eeb9
tokitou-san <tokitousan2005@gmail.com>
2023-07-28 23:48:31 +0530

    fix font config

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.bgcolor = "#03020c"
+    page.theme_mode = ft.ThemeMode.LIGHT
     page.window_width = 330
     page.window_height = 660
     # configure custom fonts
     page.fonts = {
         "Poppins": "fonts/Poppins/Poppins-regular.ttf",
+        "Fontspring": "fonts/Fontspring/Fontspring-regular.ttf",
     }
 
-    page.add(ft.Text("Instagram", font_family="Poppins"))
+    page.add(ft.Text("Instagram", font_family="Fontspring"))
 
 if __name__ == "__main__":
     ft.app(target=main, assets_dir="assets")
