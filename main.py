2c200ad6ac18d6c787388750567c2d9441906d8e
tokitou-san <tokitousan2005@gmail.com>
2023-07-28 19:52:34 +0530

    add demo button

---
+++ main.py
@@ -0,0 +1,13 @@
+import flet as ft
+
+def main(page: ft.Page):
+    # "#03020c"
+    page.add(
+        ft.FloatingActionButton(
+            icon=ft.icons.ADD_CIRCLE,
+            bgcolor=ft.colors.BLUE_500
+        )
+    )
+
+if __name__ == "__main__":
+    ft.app(target=main)
