f2eef839d3f5fb1ec0b8151c8d3c29719112137a
tokitou-san <tokitousan2005@gmail.com>
2023-07-28 23:20:09 +0530

    style: add font_family

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ def main(page: ft.Page):
     page.bgcolor = "#03020c"
     page.window_width = 330
     page.window_height = 660
-    
-    page.add(
-        ft.FloatingActionButton(
-            icon=ft.icons.ADD_CIRCLE,
-            bgcolor=ft.colors.BLUE,
-        )
-    )
+
+    page.fonts = {
+        "Poppins": "/fonts/Poppins/Poppins-regular.ttf",
+    }
+
+    page.add(ft.Text("Instagram", font_family="Poppins"))
 
 if __name__ == "__main__":
-    ft.app(target=main)
+    ft.app(target=main, assets_dir="assets")
