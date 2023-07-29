b393a2903d8dc8f3be053f2d93beaa59cf0c8319
tokitou-san <tokitousan2005@gmail.com>
2023-07-29 18:49:05 +0530

    configured padding and image

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main(page: ft.Page):
     page.theme_mode = ft.ThemeMode.LIGHT
     page.window_width = 330
     page.window_height = 660
+    page.padding = 0
     # configure custom fonts
     page.fonts = {
         "Poppins": "fonts/Poppins/Poppins-regular.ttf",
@@ -12,21 +13,36 @@ def main(page: ft.Page):
 
     def appbar():
         return (
-            ft.Row(
-                [
-                    ft.Text(
-                        "Instagram",
-                        font_family="Fontspring",
-                        size=20,
-                    ),
-                    ft.IconButton(icon=ft.icons.ADD)
-                ],
-                alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            ft.Container(
+                content = ft.Row(
+                    [
+                        ft.Text(
+                            "Instagram",
+                            font_family="Fontspring",
+                            size=20,
+                        ),
+                        ft.IconButton(icon=ft.icons.ADD)
+                    ],
+                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                ),
+                padding=10
+            )
+        )
+
+    def slider():
+        return(
+            ft.Image(
+                src=f"https://picsum.photos/200/200",
+                width=200,
+                height=200,
+                fit=ft.ImageFit.NONE,
+                repeat=ft.ImageRepeat.NO_REPEAT,
             )
         )
 
     page.add(
         appbar(),
+        slider(),
     )
 
 if __name__ == "__main__":
