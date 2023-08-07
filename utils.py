367a327c7f2cd6a881914a8a428c53820cf809b3
tokitou-san <tokitousan2005@gmail.com>
2023-08-07 21:02:18 +0530

    format: utlis components file

--- utils.py
+++ utils.py
@@ -1,23 +1,23 @@
 import flet as ft
 
 def image_loader(src: str, width: ft.OptionalNumber, height: int, border_radius: int = 0):
-    return ft.Stack(
-        [
-            ft.Container(
-                width=width,
-                height=height,
-                bgcolor = "#e7ebf4",
-                border_radius=border_radius
-            ),
-            ft.Image(
-                src=src,
-                width=width,
-                height=height,
-                fit=ft.ImageFit.COVER,
-                repeat=ft.ImageRepeat.NO_REPEAT,
-                border_radius=border_radius
-            )
-        ],
-        width=width,
-        height=height,
-    )
+   return ft.Stack(
+      [
+         ft.Container(
+            width=width,
+            height=height,
+            bgcolor = "#e7ebf4",
+            border_radius=border_radius
+         ),
+         ft.Image(
+            src=src,
+            width=width,
+            height=height,
+            fit=ft.ImageFit.COVER,
+            repeat=ft.ImageRepeat.NO_REPEAT,
+            border_radius=border_radius
+         )
+      ],
+      width=width,
+      height=height,
+   )
