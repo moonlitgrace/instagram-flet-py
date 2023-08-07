2505948f0018b9538adb02fe39ac16a448566063
tokitou-san <tokitousan2005@gmail.com>
2023-08-07 22:27:35 +0530

    change component position

--- main.py
+++ main.py
@@ -12,6 +12,45 @@ def main(page: ft.Page):
       "Fontspring": "fonts/Fontspring/Fontspring-bold.otf",
    }
 
+   appbar = ft.Container(
+      content = ft.Row(
+         [
+            ft.Text(
+               "Instagram",
+               font_family="Fontspring",
+               size=22
+            ),
+            ft.Row(
+               [
+                  ft.Container(
+                     content=ft.Image(
+                        src="icons/like-outline.svg",
+                        width=22,
+                        height=22,
+                        fit=ft.ImageFit.COVER,
+                        repeat=ft.ImageRepeat.NO_REPEAT
+                     )
+                  ),
+                  ft.Container(
+                     content=ft.Image(
+                        src="icons/messenger-outline.svg",
+                        width=22,
+                        height=22,
+                        fit=ft.ImageFit.COVER,
+                        repeat=ft.ImageRepeat.NO_REPEAT
+                     )
+                  )
+               ],
+               spacing=15
+            )
+         ],
+         alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+         vertical_alignment=ft.CrossAxisAlignment.CENTER
+      ),
+      padding=ft.padding.all(10),
+      border=ft.border.only(bottom=ft.BorderSide(1, ft.colors.with_opacity(0.05, ft.colors.BLACK)))
+   )
+
    def story_view(image: str, username: str):
       return ft.Container(
          content=ft.Column(
@@ -82,45 +121,6 @@ def main(page: ft.Page):
       ],
       spacing=5,
       horizontal_alignment=ft.CrossAxisAlignment.CENTER
-   )
-
-   appbar = ft.Container(
-      content = ft.Row(
-         [
-            ft.Text(
-               "Instagram",
-               font_family="Fontspring",
-               size=22
-            ),
-            ft.Row(
-               [
-                  ft.Container(
-                     content=ft.Image(
-                        src="icons/like-outline.svg",
-                        width=22,
-                        height=22,
-                        fit=ft.ImageFit.COVER,
-                        repeat=ft.ImageRepeat.NO_REPEAT
-                     )
-                  ),
-                  ft.Container(
-                     content=ft.Image(
-                        src="icons/messenger-outline.svg",
-                        width=22,
-                        height=22,
-                        fit=ft.ImageFit.COVER,
-                        repeat=ft.ImageRepeat.NO_REPEAT
-                     )
-                  )
-               ],
-               spacing=15
-            )
-         ],
-         alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-         vertical_alignment=ft.CrossAxisAlignment.CENTER
-      ),
-      padding=ft.padding.all(10),
-      border=ft.border.only(bottom=ft.BorderSide(1, ft.colors.with_opacity(0.05, ft.colors.BLACK)))
    )
 
    stories = ft.Container(
