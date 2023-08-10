18cda15073c4a962216e011675aa6c95c605a19d
tokitou-san <tokitousan2005@gmail.com>
2023-08-11 05:18:22 +0530

    create icon component for custom icons

--- libs/components/navigation_bar.py
+++ libs/components/navigation_bar.py
@@ -1,52 +1,13 @@
 import flet as ft
+from utils import icon
 
 navigation_bar = ft.Container(
 	content=ft.Row([
-		ft.Container(
-	        content=ft.Image(
-	            src="icons/home.svg",
-	            width=22,
-	            height=22,
-	            fit=ft.ImageFit.COVER,
-	           	repeat=ft.ImageRepeat.NO_REPEAT
-	        )
-	    ),
-	    ft.Container(
-	        content=ft.Image(
-	            src="icons/search.svg",
-	            width=25,
-	            height=25,
-	            fit=ft.ImageFit.COVER,
-	           	repeat=ft.ImageRepeat.NO_REPEAT
-	        )
-	    ),
-	    ft.Container(
-	        content=ft.Image(
-	            src="icons/add_box.svg",
-	            width=25,
-	            height=25,
-	            fit=ft.ImageFit.COVER,
-	           	repeat=ft.ImageRepeat.NO_REPEAT
-	        )
-	    ),
-	    ft.Container(
-	        content=ft.Image(
-	            src="icons/reel.svg",
-	            width=18,
-	            height=18,
-	            fit=ft.ImageFit.COVER,
-	           	repeat=ft.ImageRepeat.NO_REPEAT
-	        )
-	    ),
-	    ft.Container(
-	        content=ft.Image(
-	            src="icons/user.svg",
-	            width=22,
-	            height=22,
-	            fit=ft.ImageFit.COVER,
-	           	repeat=ft.ImageRepeat.NO_REPEAT
-	        )
-	    ),
+	    icon(src="icons/home.svg", width=22, height=22),
+	    icon(src="icons/search.svg", width=25, height=25),
+	    icon(src="icons/add_box.svg", width=22, height=22),
+	    icon(src="icons/reel.svg", width=18, height=18),
+	    icon(src="icons/user.svg", width=22, height=22),
 	],
 	alignment=ft.MainAxisAlignment.SPACE_AROUND),
 	padding=ft.padding.all(10)
--- utils.py
+++ utils.py
@@ -21,3 +21,14 @@ def image_loader(src: str, width: ft.OptionalNumber, height: int, border_radius:
       width=width,
       height=height,
    )
+
+def icon(src: str, width: int, height: int) -> ft.Container:
+   return ft.Container(
+      content=ft.Image(
+         src=src,
+         width=width,
+         height=height,
+         fit=ft.ImageFit.COVER,
+         repeat=ft.ImageRepeat.NO_REPEAT
+      )
+   )
