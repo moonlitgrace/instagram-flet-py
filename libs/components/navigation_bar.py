b200bd8887d39d388370c3f630850f3d11c8247b
tokitou-san <tokitousan2005@gmail.com>
2023-08-11 04:46:42 +0530

    add home icon

--- libs/components/navigation_bar.py
+++ libs/components/navigation_bar.py
@@ -1,3 +1,16 @@
 import flet as ft
 
-navigation_bar = ft.Text("navigation_bar")
+navigation_bar = ft.Container(
+	content=ft.Row([
+		ft.Container(
+	        content=ft.Image(
+	            src="icons/home.svg",
+	            width=22,
+	            height=22,
+	            fit=ft.ImageFit.COVER,
+	           	repeat=ft.ImageRepeat.NO_REPEAT
+	        )
+	     ),
+	]),
+	padding=ft.padding.all(10)
+)
