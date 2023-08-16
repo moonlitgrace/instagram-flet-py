605a46509870329c31d2173410a2fa0acddbef4d
tokitou-san <tokitousan2005@gmail.com>
2023-08-16 05:45:53 +0530

    major refactor: appbar

--- libs/components/appbar.py
+++ libs/components/appbar.py
@@ -5,23 +5,31 @@ class Appbar(ft.Container):
    def __init__(self, *args, **kwargs):
       super(Appbar, self).__init__(*args, **kwargs)
 
-      self.content = ft.Row(
+      self.content = self._create_appbar()
+      self.padding = ft.padding.all(10)
+
+   def _create_appbar(self):
+      return ft.Row(
          [
-            ft.Text(
-               "Instagram",
-               font_family="Fontspring",
-               size=22
-            ),
-            ft.Row(
-               [
-                  icon(src="icons/like-outline.svg", width=22, height=22),
-                  icon(src="icons/messenger-outline.svg", width=22, height=22),
-               ],
-               spacing=15
-            )
+            self._create_app_name(),
+            self._create_icons()
          ],
          alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
          vertical_alignment=ft.CrossAxisAlignment.CENTER
       )
 
-      self.padding = ft.padding.all(10)
+   def _create_app_name(self):
+      return ft.Text(
+         "Instagram",
+         font_family="Fontspring",
+         size=22
+      )
+
+   def _create_icons(self):
+      return ft.Row(
+         [
+            icon(src="icons/like-outline.svg", width=22, height=22),
+            icon(src="icons/messenger-outline.svg", width=22, height=22),
+         ],
+         spacing=15
+      )
