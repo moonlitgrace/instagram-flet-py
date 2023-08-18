5d9239d6cb328e62130f49e4669f71e9380ecf6c
tokitou-san <tokitousan2005@gmail.com>
2023-08-18 07:35:30 +0530

    refactor (appbar): better implementation

--- libs/components/appbar.py
+++ libs/components/appbar.py
@@ -5,27 +5,27 @@ class Appbar(ft.Container):
    def __init__(self, *args, **kwargs) -> None:
       super().__init__(*args, **kwargs)
 
-      self.content = self.__create_appbar()
+      self.content = self._build_appbar()
       self.padding = ft.padding.all(10)
 
-   def __create_appbar(self) -> ft.Control:
+   def _build_appbar(self) -> ft.Control:
       return ft.Row(
          [
-            self.__create_app_name(),
-            self.__create_icons()
+            self._build_app_name(),
+            self._build_icons()
          ],
          alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
          vertical_alignment=ft.CrossAxisAlignment.CENTER
       )
 
-   def __create_app_name(self) -> ft.Control:
+   def _build_app_name(self) -> ft.Control:
       return ft.Text(
          "Instagram",
          font_family="Fontspring",
          size=22
       )
 
-   def __create_icons(self) -> ft.Control:
+   def _build_icons(self) -> ft.Control:
       return ft.Row(
          [
             Icon(src="icons/like-outline.svg", width=22, height=22),
